8d7d307e1b6225504eaf879f889efefc2db7e6d7
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-07-08 11:50:48 +0700

    Menyelesaikan generic class

---
+++ src/kotlin01Introduction/Generic.kt
@@ -0,0 +1,26 @@
+package kotlin01Introduction
+
+class MutableStack<E>(vararg items: E){
+    private val elements = items.toMutableList()
+    fun push(element: E) = elements.add(element)
+    fun peek(): E = elements.last()
+    fun pop(): E = elements.removeAt(elements.size - 1)
+    fun isEmpty() = elements.isEmpty()
+    fun size() = elements.size
+
+    @Override
+    override fun toString() = "MutableStack(${elements.joinToString()})"
+
+}
+fun <E> mutableStackOf(vararg elements: E) = MutableStack(*elements)
+
+fun main() {
+    val stack = MutableStack("Adam", 17, true)
+    val stack2 = mutableStackOf(1,2,3,4,true, false)
+    println(stack.toString())
+    stack.push(192)
+    println(stack.toString())
+    stack.push(168)
+    println(stack.toString())
+    println(stack2)
+}
